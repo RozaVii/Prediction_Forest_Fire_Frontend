3a603d8331f5062a55ac441e50d71c785051f0e8
Станислав Цветков <schvetkov@dvfu.ru>
2024-07-07 19:23:50 +1000

    19:23 Очистка БД и хедер отображение пользователя

--- src/components/DatabaseCleanup.js
+++ src/components/DatabaseCleanup.js
@@ -1,20 +1,18 @@
-// src/components/DatabaseCleanup.js
+  // src/components/DatabaseCleanup.js
 import React, { useState } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import './DatabaseCleanup.css';
+import {useAxios} from "./AxiosComponent";
 
 const DatabaseCleanup = ({ userRole }) => {
   const [message, setMessage] = useState('');
+  const axios = useAxios('http://localhost:8080');
 
   const handleCleanup = () => {
-    if (userRole !== 'admin') {
-      setMessage('У вас недостаточно прав для выполнения этой операции.');
-      return;
-    }
-
-    // Здесь должен быть код для очистки базы данных
-    setMessage('База данных успешно очищена.');
+    axios.post('/api/v1/app-endpoints')
+        .then(_ => setMessage('База данных успешно очищена.'))
+        .catch((err) => console.log(err))
   };
 
   return (
@@ -26,7 +24,7 @@ const DatabaseCleanup = ({ userRole }) => {
           <div className="cleanup-box">
             <h1>Вы уверены что хотите очистить базу?</h1>
             <p>При очистке базы произойдет полное удаление всех данных без возможности восстановления</p>
-            <button onClick={handleCleanup} disabled={userRole !== 'admin'}>
+            <button onClick={handleCleanup}>
               Удалить базу
             </button>
             {message && <p className="message">{message}</p>}
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,23 @@
 import React from 'react';
 import './Header.css';
 import logo from '../images/logo-2.png';
+import {jwtDecode} from "jwt-decode";
 
 const Header = () => {
+
+    const roleMap = {
+        "ADMINISTRATOR": "Администратор",
+        "SPECIALIST": "Специалист"
+    }
+
+    const getUser = () => {
+        const accessToken = localStorage.getItem('accessToken');
+        const decodedToken = jwtDecode(accessToken);
+
+        return {username: decodedToken.sub, role: roleMap[decodedToken.role]}
+    }
+
+    const user = getUser()
   return (
     <div className="header">
       <div className="header-left">
@@ -11,7 +26,7 @@ const Header = () => {
         <h1>Prediction Forest Fire</h1>
       </div>
       <div className="header-right">
-        <span>Роль: Администратор</span>
+        <span>{user.username + " : " + user.role}</span>
       </div>
     </div>
   );
